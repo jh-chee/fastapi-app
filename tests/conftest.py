d567fa2febce69e02e0333950cdb6ca099d0fddd
jh-chee <hardfastfurioushff@gmail.com>
2021-11-24 17:42:19 +0800

    edit test database

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ from app.config import settings
 from app import models
 # from alembic import command
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine)
